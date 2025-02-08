2ee7b6177194a88abfae27bb869d802e3b8613f4
thanley11 <thanley11@gmail.com>
2025-02-07 19:22:11 -0500

    Update EDLS-FS.ino

--- EDLS-FS.ino
+++ EDLS-FS.ino
@@ -1,79 +1,88 @@
-/******************************
-   EDLS-FS v1.0
-   for Teensy LC (www.pjrc.com)
-   by Notes and Volts
-   www.notesandvolts.com
- ******************************/
-
-/******************************
-   ** Upload Settings **
-   Board: "Teensy LC"
-   USB Type: "MIDI"
-   CPU Speed: "48 Mhz"
- ******************************/
-
 #include <Bounce.h>
 
-//MIDI Channel
+// MIDI Channel
 #define MIDI_CHANNEL 1
 
-//Switch CC Numbers
+// Switch CC Numbers (for L1 & L2)
 #define LOOP1 38
 #define LOOP2 39
 #define LOOP4 40
 #define LOOP8 41
 
-//Pin Numbers on Teensy
+// Program Change Range
+#define MIN_PROGRAM 0  
+#define MAX_PROGRAM 10  // Set max allowed program changes
+
+// Pin Numbers on Teensy
 #define L1_PIN 11
 #define L2_PIN 10
-#define L4_PIN 2
-#define L8_PIN 3
+#define L4_PIN 2  // Decrease Program Change
+#define L8_PIN 3  // Increase Program Change
 
-//Other
-#define SWITCHES 4 // How many switches?
-#define VELOCITY 127 // ON Velocity
-#define BOUNCE_TIME 15 // Debounce Time (in milliseconds)
-//********************
+// Other
+#define SWITCHES 4  // Total number of switches
+#define VELOCITY 127 // CC ON value
+#define BOUNCE_TIME 15  // Debounce Time (in milliseconds)
 
-//Global variables
+// Global variables
 const int DIGITAL_SWITCHES[SWITCHES] = {L1_PIN, L2_PIN, L4_PIN, L8_PIN};
 const int CC_NUMBER[SWITCHES] = {LOOP1, LOOP2, LOOP4, LOOP8};
 
-//Set up 'Bounce' Objects
-Bounce digital[] =   {
+// Track the current Program Change
+int currentProgram = 0;  
+
+// Set up 'Bounce' Objects
+Bounce digital[] = {
   Bounce(DIGITAL_SWITCHES[0], BOUNCE_TIME),
   Bounce(DIGITAL_SWITCHES[1], BOUNCE_TIME),
   Bounce(DIGITAL_SWITCHES[2], BOUNCE_TIME),
   Bounce(DIGITAL_SWITCHES[3], BOUNCE_TIME),
 };
 
-//Setup
+// Setup
 void setup() {
   for (int i = 0; i < SWITCHES; i++) {
     pinMode(DIGITAL_SWITCHES[i], INPUT_PULLUP);
   }
 }
 
-//Main Loop
+// Main Loop
 void loop() {
   readSwitches();
   while (usbMIDI.read()) {
   }
 }
 
-//Read Buttons
+// Read Buttons
 void readSwitches() {
   static byte input = 0;
 
   digital[input].update();
 
   if (digital[input].fallingEdge()) {
-    usbMIDI.sendControlChange(CC_NUMBER[input], VELOCITY, MIDI_CHANNEL);
+    if (input < 2) {
+      // Send Control Change (L1 & L2)
+      usbMIDI.sendControlChange(CC_NUMBER[input], VELOCITY, MIDI_CHANNEL);
+    } else {
+      // Program Change Handling (Flip behavior)
+      if (input == 2 && currentProgram < MAX_PROGRAM) {
+        currentProgram++;  // L4 now increases program
+      } 
+      else if (input == 3 && currentProgram > MIN_PROGRAM) {
+        currentProgram--;  // L8 now decreases program
+      }
+      
+      usbMIDI.sendProgramChange(currentProgram, MIDI_CHANNEL);
+    }
   }
   else if (digital[input].risingEdge()) {
-    usbMIDI.sendControlChange(CC_NUMBER[input], 0, MIDI_CHANNEL);
+    if (input < 2) {
+      // Send CC OFF (value 0) for L1 & L2, but ignore for L4 & L8
+      usbMIDI.sendControlChange(CC_NUMBER[input], 0, MIDI_CHANNEL);
+    }
   }
 
   input++;
   if (input >= SWITCHES) input = 0;
 }
+
